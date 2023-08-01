7104835c05284665dc9465d5df57482bd3947d9a
Ellisvelandia <eyis619l@gmail.com>
2023-07-31 22:50:15 -0500

    grade score if else

--- compare.py
+++ compare.py
@@ -1,9 +1,19 @@
 x = int(input("whats x? "))
 y = int(input("whats y? "))
 
-if x < y:
-  print("x is less than y")
-elif x > y:
-  print("x is greater than y")
+# if x < y:
+#   print("x is less than y")
+# elif x > y:
+#   print("x is greater than y")
+# else:
+#   print("x is equal to y")
+
+# if x < y or x > y:
+#     print("x is not equal to y")
+# else:
+#     print("x is equal to y")
+
+if x != y:
+    print("x is equal to y")
 else:
-  print("x is equal to y")
+    print("x is not equal to y")
