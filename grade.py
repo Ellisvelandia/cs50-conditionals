cc0413e775c582ec015ddd561570ecbc3cd1f061
Ellisvelandia <eyis619l@gmail.com>
2023-07-31 22:50:46 -0500

    else if grade

--- grade.py
+++ grade.py
@@ -11,3 +11,26 @@ score = int(input("Score: "))
 # else:
 #     print("Grade: F")                
 
+
+# if score <= 90 <= 100:
+#     print("Grade: A")
+# elif score <= 80 < 90:
+#     print("Grade: B")
+# elif score <= 70 < 80:
+#     print("Grade: C")
+# elif score <= 60 < 70:
+#     print("Grade: D")
+# else:
+#     print("Grade: F")                
+
+
+if score >= 90:
+    print("Grade: A")
+elif score >= 80:
+    print("Grade: B")
+elif score >= 70:
+    print("Grade: C")
+elif score >= 60:
+    print("Grade: D")
+else:
+    print("Grade: F")                
